c6943d19598cde9f2d03948d733b5bfbca306aee
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-02-20 17:59:30 -0500

    Updated Workflow

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ resource "google_compute_network" "vpc_network" {
     delete_default_routes_on_create = var.delete_default_routes_on_create
     enable_ula_internal_ipv6 = var.enable_ula_internal_ipv6
     mtu = var.maximum_transmission_unit
-}
 
 # Create two subnets in the VPC network
 #Subnet 1 - For webapp. This subnet will have access to internet
