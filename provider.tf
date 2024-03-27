48c7d27cfb703fca38b98ccc22b77295b00a44f9
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-03-27 13:31:13 -0400

    Added resources

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 #Provider - GCP
 
 provider "google" {
-  credentials = file(var.gcp_credentials_file)
+  #credentials = file(var.gcp_credentials_file)
   project     = var.gcp_project
   region      = var.gcp_region
 }
