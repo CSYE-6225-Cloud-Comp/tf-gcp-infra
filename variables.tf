0e7d6d2fc90a31ec60c6446d1d5ee8da3488fd95
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-04-03 17:47:15 -0400

    Added variables

--- variables.tf
+++ variables.tf
@@ -426,11 +426,11 @@ variable "max_instance_count" {
   type = number
 }
 
-variable private_ip_google_access_db {
+variable "private_ip_google_access_db" {
   type = bool
 }
 
-variable autoscaler_cooldown_period {
+variable "autoscaler_cooldown_period" {
   type = number
 }
 
@@ -438,54 +438,210 @@ variable "autoscaler_cpu_utilization" {
   type = number
 }
 
-variable autoscaler_min_replicas {
+variable "autoscaler_min_replicas" {
   type = number
 }
 
-variable autoscaler_max_replicas {
+variable "autoscaler_max_replicas" {
   type = number
 }
 
-variable instance_template_machine {
+variable "instance_template_machine" {
   type = string
 }
 
-variable ingress_source_ranges {
+variable "ingress_source_ranges" {
   type = list(string)
 }
 
-variable load_balancing_region {
+variable "load_balancing_region" {
   type = string
 }
 
-variable domains {
+variable "domains" {
   type = list(string)
 }
 
-variable ssl_certificate_name {
+variable "ssl_certificate_name" {
   type = string
 }
 
-variable instance_template_name {
+variable "instance_template_name" {
   type = string
 }
 
-variable instance_template_mode {
+variable "instance_template_mode" {
   type = string
 }
 
-variable instance_template_labels {
+variable "instance_template_labels" {
   type = string
 }
 
-variable full_scope {
+variable "full_scope" {
   type = list(string)
 }
 
-variable autoscaler_name {
+variable "autoscaler_name" {
   type = string
 }
 
 variable "health_check_name" {
+  type = string
+}
+
+variable "forwarding_rule_name" {
+  type = string
+}
+
+variable "forwarding_rule_ip_protocol" {
+  type = string
+}
+
+variable "forwarding_rule_port_range" {
+  type = string
+}
+
+variable "forwarding_load_balancing_scheme" {
+  type = string
+}
+
+variable "https_proxy_name" {
+  type = string
+}
+
+variable "url_map_name" {
+  type = string
+}
+
+variable "backend_service_name" {
+  type = string
+}
+
+variable "backend_service_connection_draining_timeout_sec" {
+  type = number
+}
+
+variable "backend_load_balancing_scheme" {
+  type = string
+}
+
+variable "backend_port_name" {
+  type = string
+}
+
+variable "backend_protocol" {
+  type = string
+}
+
+variable "backend_session_affinity" {
+  type = string
+}
+
+variable "backend_timeout_sec" {
+  type = number
+}
+
+variable "backend_balancing_mode" {
+  type = string
+}
+
+variable "backend_capacity_scaler" {
+  type = number
+}
+
+variable "health_check_check_interval_sec" {
+  type = number
+}
+
+variable "health_check_healthy_threshold" {
+  type = number
+}
+
+variable "health_check_port" {
+  type = number
+}
+
+variable "health_check_request_path" {
+  type = string
+}
+
+variable "health_check_timeout_sec" {
+  type = number
+}
+
+variable "health_check_unhealthy_threshold" {
+  type = number
+}
+
+variable "compute_global_address_name" {
+  type = string
+}
+
+variable "compute_global_address_name_ip_version" {
+  type = string
+}
+
+variable "pubsub_publisher_role" {
+  type = string
+}
+
+variable "instance_template_automatic_template" {
+  type = bool
+}
+
+variable "instance_template_automatic_template_on_host_maintenance" {
+  type = string
+}
+
+variable "instance_template_automatic_template_provisioning_model" {
+  type = string
+}
+
+variable "managed_instance_group_name" {
+  type = string
+}
+
+variable "managed_instance_named_port_name" {
+  type = string
+}
+
+variable "managed_instance_named_port_number" {
+  type = string
+}
+
+variable "managed_instance_group_version_name" {
+  type = string
+}
+
+variable "base_instance_name" {
+  type = string
+}
+
+variable "auto_healing_policies_initial_delay_sec" {
+  type = number
+}
+
+variable "health_check_firewall_name" {
+  type = string
+}
+
+variable "health_check_firewall_direction" {
+  type = string
+}
+
+variable "health_check_firewall_priority" {
+  type = number
+}
+
+variable "health_check_firewall_target_tags" {
+  type = list(string)
+}
+
+variable "health_check_firewall_allow_port" {
+  type = list(string)
+}
+
+variable "health_check_firewall_protocol" {
   type = string
 }
